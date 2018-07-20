d9e1b6fbc2def94431370295e8c7e89b8b48740e
jbphet <john.blanco@colorado.edu>
2018-07-20 10:14:23 -0600

    added the tambo sound library as a dependency for all active runnables, see https://github.com/phetsims/joist/issues/497

--- js/circuit-construction-kit-dc-config.js
+++ js/circuit-construction-kit-dc-config.js
@@ -40,6 +40,7 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
+    TAMBO: '../../tambo/js',
     TANDEM: '../../tandem/js',
     TWIXT: '../../twixt/js'
   },
