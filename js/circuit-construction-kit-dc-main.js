477d1791ca3cad1e1d6e4c1b534dc03355d29d59
zepumph <michael.kauzmann@colorado.edu>
2021-03-09 08:26:41 -0900

    comment with import, https://github.com/phetsims/circuit-construction-kit-dc/issues/190

--- js/circuit-construction-kit-dc-main.js
+++ js/circuit-construction-kit-dc-main.js
@@ -7,10 +7,10 @@
  */
 
 import CCKCConstants from '../../circuit-construction-kit-common/js/CCKCConstants.js';
-// Image is required for making toDataURLNodeSynchronous work in the built version
 import CCKCOptionsDialogContent from '../../circuit-construction-kit-common/js/view/CCKCOptionsDialogContent.js';
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+// Image is required for making toDataURLNodeSynchronous work in the built version
 import '../../scenery/js/nodes/Image.js';
 import soundManager from '../../tambo/js/soundManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
