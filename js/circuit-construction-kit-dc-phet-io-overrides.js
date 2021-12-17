422f7d4c2339235ea418796e7c5d58881a72933a
Sam Reid <reids@colorado.edu>
2021-12-17 12:05:36 -0700

    Remove overrides

--- js/circuit-construction-kit-dc-phet-io-overrides.js
+++ js/circuit-construction-kit-dc-phet-io-overrides.js
@@ -1,25 +1,2 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides =
-  {
-    "circuitConstructionKitDc.introScreen.model.ammeter1.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "circuitConstructionKitDc.introScreen.model.ammeter2.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "circuitConstructionKitDc.introScreen.model.circuit.showCurrentProperty": {
-      "phetioFeatured": true
-    },
-    "circuitConstructionKitDc.introScreen.view.circuitElementToolbox.carousel.tools.resistorToolNode.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "circuitConstructionKitDc.introScreen.view.sensorToolbox.ammeterToolIconWithLabel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "circuitConstructionKitDc.introScreen.view.sensorToolbox.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "circuitConstructionKitDc.introScreen.view.sensorToolbox.voltmeterToolIconWithLabel.visibleProperty": {
-      "phetioFeatured": true
-    }
-  };
+window.phet.preloads.phetio.phetioElementsOverrides = {};
