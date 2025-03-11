c86e9be0b9d389892e6eef6805cd32308f2c2910
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-11 16:40:48 -0600

    create a focusable fallback for delete key support in CCK, https://github.com/phetsims/circuit-construction-kit-common/issues/1027

--- js/circuit-construction-kit-dc-main.ts
+++ js/circuit-construction-kit-dc-main.ts
@@ -11,6 +11,7 @@ import CCKCSimulationPreferencesContentNode from '../../circuit-construction-kit
 import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+import affirm from '../../perennial-alias/js/browser-and-node/affirm.js';
 import soundManager from '../../tambo/js/soundManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import CircuitConstructionKitDcStrings from './CircuitConstructionKitDcStrings.js';
@@ -53,6 +54,19 @@ simLauncher.launch( () => {
     phetioDesigned: true
   } );
   sim.start();
+
+  // TODO: Make a CCKCSim() that factors this out? https://github.com/phetsims/circuit-construction-kit-common/issues/1027
+  const element = sim.display.getFocusablePlaceholder();
+  affirm( element );
+  sim.display.addInputListener( {
+    down: () => {
+
+      // Only if there isn't another item focused already.
+      if ( document.activeElement === document.body ) {
+        element.focus();
+      }
+    }
+  } );
 
   // Disable sounds for joist/home screen/navigation bar/carousel, but leave sound for the dog bark
   soundManager.setOutputLevelForCategory( 'user-interface', 0 );
